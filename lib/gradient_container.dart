64d961d6b93930e262172a16ebe7953da21c2b96
susboi <aztahir20@gmail.com>
2024-08-15 12:51:38 +0100

    added dynamic text

---
+++ lib/gradient_container.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:first_app/styled_text.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 255, 150, 3),
+            Color.fromARGB(255, 255, 0, 0)
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(child: StyledText('Hello World!')),
+    );
+  }
+}
