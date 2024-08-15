fdb185f93cd58e36ef8588d0565f39a5350bb793
susboi <aztahir20@gmail.com>
2024-08-15 12:01:51 +0100

    new color

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,36 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [Colors.white10, Colors.black38]),
-          ),
-          child: const Center(
-            child: Text('Hello World!'),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 255, 150, 3),
+            Color.fromARGB(255, 255, 0, 0)
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello World!',
+          style: TextStyle(
+              color: Color.fromARGB(255, 255, 255, 255), fontSize: 28),
+        ),
+      ),
+    );
+  }
 }
